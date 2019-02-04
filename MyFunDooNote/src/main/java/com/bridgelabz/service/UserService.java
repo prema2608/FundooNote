124d274d0fc7e564b9e1d5fe8434aa8184723190
prema2608 <prema.s2608@gmail.com>
2019-02-04 19:26:06 +0530

    first commit

---
+++ MyFunDooNote/src/main/java/com/bridgelabz/service/UserService.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.service;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.bridgelabz.model.User;
+
+public interface UserService {
+	public boolean register(User user, HttpServletRequest request);
+
+	public User loginUser(String emailId, String password, HttpServletRequest request);
+
+	public User updateUser(int id, User user, HttpServletRequest request);
+
+	public User deleteUser(int id, HttpServletRequest request);
+
+}
